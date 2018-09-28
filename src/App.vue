5f36ad0633c0b3e702c7df55f3790d4a6bee0fd0
Robert Halter <roebi@halter.in>
2018-09-28 22:48:16 +0200

    first File List

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <FileExplorer msg="File Explorer"/>
+    <FileExplorer/>
   </div>
 </template>
 
@@ -24,5 +23,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+    background-color: #eeeeee;
 }
 </style>
--- src/components/FileExplorer.vue
+++ src/components/FileExplorer.vue
@@ -1,59 +1,82 @@
 <template>
   <div class="filexplorer">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{ name }}</h1>
+    <div class="holder">
+      <ul>
+        <li v-for="(file, index) in files" :key='index'>{{index}}. ({{file.type}}) {{file.name}}</li>
+      </ul>
+      <p v-if="files.length > 1">You have {{files.length}} files</p>
+      <p v-else>You have less than or equal to 1 file</p>
+      <!-- <div v-bind:class="{ alert: showAlert }">vue class binding</div> -->
+      <!-- <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }">vue style binding</div> -->
+      <!-- <div v-bind:style="alertObject">vue style binding to object</div> does not work -->
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'FileExplorer',
-  props: {
-    msg: String
+  data() {
+    return {
+      name: 'File Explorer',
+      files: [
+          { "name": "ZT_AUTOMAT_T",
+            "type": "dir"
+          },
+          { "name": "ZT_FACHTEST_T",
+            "type": "file"
+          }
+      ],
+      showAlert: true,
+      bgColor: 'yellow',
+      bgWidth: '100%',
+      bgHeight: '30px',
+      alertObject: {
+        bgColor: 'yellow',
+        bgWidth: '100%',
+        bgHeight: '30px'
+      },
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- <style src="./FileExplorer.css" scoped> File is in src/components/FileExplorer.css -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.alert {
+    background-color: yellow;
+    width:100%;
+    height: 30px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+.holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
 </style>
