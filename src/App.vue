3c7a974ec80ad5eb76558c11655b36a0387eb4d3
Robert Halter <roebi@halter.in>
2018-10-02 09:28:29 +0200

    router-link as el-button with tag attribute.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">File Explorer</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link to="/" tag="el-button">File Explorer</router-link>
+      <router-link to="/about" tag="el-button">About</router-link>
     </nav>
     <router-view/>
   </div>
