4f6a21f05916cc9303e5a24910f501f07e12e154
Robert Halter <roebi@halter.in>
2018-10-02 11:56:30 +0200

    vue-i18n not active

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
+// import VueI18n from 'vue-i18n'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en'
+import enLocale from 'element-ui/lib/locale/lang/en'
+import deLocale from 'element-ui/lib/locale/lang/de'
 
-Vue.use(Element, { locale })
+// Vue.use(VueI18n); // , { Locale: 'en' });
+Vue.use(Element, { enLocale });
+
+// Vue.config.lang = 'de';
+// VueI18n.Locale = 'en';
+// Vue.locale('en', enLocale);
+// Vue.locale('de', deLocale);
