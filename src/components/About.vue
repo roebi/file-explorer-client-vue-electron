25b929b314f205b46e6312dd568b2e09d357f880
Robert Halter <roebi@halter.in>
2018-10-02 09:05:48 +0200

    el-button from Element added.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="about">
     <h1>Hello {{ their_name }}, this is about page</h1>
+    <img src="../assets/logo.png">
     <p>File Explorer - Lorem ipsum dolor sit amet, elit, sed do tempor</p>
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+
   </div>
 </template>
 
